e379be19b865b6713b10926401ecc99336dcb492
bastianrust <bastian.bavaresco@valentino.com>
2015-08-19 12:54:58 +0200

    Setup Trevis

--- spec/rspec_blog_spec.rb
+++ spec/rspec_blog_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'The truth' do
 	it 'should be truthy' do
 		expect(true).to be_truthy
-		expect(true).to be_eq true
+		expect(true).to be_eql true
 	end
 
 	it 'should not be falsey' do
